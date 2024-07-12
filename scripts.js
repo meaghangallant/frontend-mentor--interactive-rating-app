20f57b2bf59fd869f538464b67bd31b2807bf2e1
meaghangallant <hello@meaghangallant.com>
2024-07-12 09:16:16 -0400

    Added JS to select rating 1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+//Select rating 1
+function selectRating() {
+  let rate1 = document.getElementById('rate-1');
+  rate1.style.color = 'var(--very-dark-blue)';
+  rate1.style.backgroundColor = 'var(--white)';
+}
