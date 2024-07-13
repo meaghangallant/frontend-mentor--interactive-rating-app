35e0eef257cb7bd9e6046373bc338e4f545d1e0d
meaghangallant <hello@meaghangallant.com>
2024-07-13 15:03:52 -0400

    adding JS to change states on button click

--- scripts.js
+++ scripts.js
@@ -1,21 +1,37 @@
-// Selected rating id >>> #selectedRating
-
+// --- Select A Rating Component ------------------------
 const getElements = document.querySelectorAll(".rating")
-console.log(getElements)
+
+const deselect = (input) => {
+  for (let i = 0; i < 5; i++){
+    if (i !== input) {
+      getElements[i].id = ""
+    }
+  }
+}
 
 getElements.forEach(rating => {
   rating.addEventListener("click", () => {
     let ratingArray = Array.from(getElements);
     let num = ratingArray.indexOf(rating);
-  
-    console.log(num);
-    console.log(getElements[num])
-    
-
+    getElements[num].id = "selectedRating";
+    deselect(num)
   })
 })
 
+// --- Switch States on Button Click --------------------
+let submitButton = document.querySelector("button")
 
+submitButton.addEventListener("click", () => {
+  let ratingState = document.querySelector("#rating-state")
+  let thankYouState = document.querySelector("#thank-you-state")
+  console.log(ratingState)
+  console.log(thankYouState)
+
+  ratingState.style.display = "none"
+  thankYouState.style.display = "block"
+})
+
+// --- Add User's Rating to Thank You State ------------
 
 
 
